1f1c489b34824752f3e44ac0b1aa39020130ea4a
Nashwan Abdullah <35005761+codenashwan@users.noreply.github.com>
2023-05-01 21:58:33 +0300

    Update permission.php

--- middleware/permission.php
+++ middleware/permission.php
@@ -5,12 +5,13 @@ namespace App\Http\Middleware;
 use Closure;
 use Illuminate\Http\Request;
 use Auth;
+
 class permission
 {
-    
+
     public function handle(Request $request, Closure $next)
     {
-        if(!in_array($request->route()->getName(), Auth::user()->routes)){
+        if (Auth::user()->routes == null || !in_array($request->route()->getName(), Auth::user()->routes)) {
             return abort(404);
         }
         return $next($request);
